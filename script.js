5f21c0f55f253f207edaad1918cdbb80b1822f22
mmm-uemura <94156239+mmm-uemura@users.noreply.github.com>
2022-02-08 13:06:34 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+$(function(){
+
+$('#modal-btn').click(function(){
+  $('.login-modal-wrapper').fadeIn();
+})
+
+$('#submit-btn').click(function(){
+  $('.login-modal-wrapper').fadeOut();
+})
+
+$('.copy').ready(function(){
+  $('h1').fadeIn(2000);
+});
+
+$(window).scroll(function() {
+  $('.fadein').each(function() {
+    let scroll = $(window).scrollTop();
+    let target = $(this).offset().top;
+    let windowHeight = $(window).height();
+    if (scroll > target - windowHeight +2) {
+      $(this).css('opacity','1');
+      $(this).css('transform','translateY(0)');
+    }
+  });
+});
+
+
+
+
+});
